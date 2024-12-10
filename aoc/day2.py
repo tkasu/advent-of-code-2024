9fb62be410e206c963efd153f2e62124e3c2d37f
Tomi Kasurinen <tomi.kasurinen@gmail.com>
2024-12-10 14:25:36 +0200

    Day2 part 2

--- aoc/day2.py
+++ aoc/day2.py
@@ -1,3 +1,5 @@
+import timeit
+
 import numpy as np
 import pandas as pd
 import torch
@@ -21,28 +23,65 @@ def read_input() -> torch.Tensor:
 
 def part1(input_tensor: torch.Tensor) -> int:
     diffs = torch.diff(input_tensor, dim=1)
-    diffs_all_inc_or_dec = diffs[
-        torch.all(diffs.isnan() | (diffs > 0), dim=1)
-        | torch.all(diffs.isnan() | (diffs < 0), dim=1)
-    ]
-    abs_diffs = torch.abs(diffs_all_inc_or_dec)
-    gradual_shift = torch.all(abs_diffs.isnan() | (abs_diffs <= 3), dim=1)
-    return gradual_shift.sum().item()
+    diff_is_consistent_mask = torch.all(diffs.isnan() | (diffs > 0), dim=1) | torch.all(
+        diffs.isnan() | (diffs < 0), dim=1
+    )
+    diff_abs_not_too_large_mask = torch.all(
+        diffs.isnan() | (torch.abs(diffs) <= 3), dim=1
+    )
+    is_valid_mask = diff_is_consistent_mask & diff_abs_not_too_large_mask
+    return is_valid_mask.sum().item()
+
+
+def create_combinations_with_drops(tensor_row: torch.Tensor) -> torch.Tensor:
+    """
+    Create a matrix of from a signle dimensional tensor with one element dropped in each row".
+    Dropped column is sequential, so column 0 is dropped for the first row, column 1 for the second row, etc.
+    """
+    filter_placeholder = (
+        np.inf
+    )  # Can't be NaN, as those are are needed for padding the tensor
+
+    tensor_row_matrix = tensor_row.repeat(tensor_row.shape[0], 1)
+    tensor_row_matrix = tensor_row_matrix.fill_diagonal_(filter_placeholder)
+    tensor_row_with_drops = tensor_row_matrix[
+        tensor_row_matrix != filter_placeholder
+    ].reshape(tensor_row.shape[0], tensor_row.shape[0] - 1)
+    return tensor_row_with_drops
 
 
 def part2(input_tensor: torch.Tensor) -> int:
-    pass
+    rows_with_drops_tensor = torch.stack(
+        [create_combinations_with_drops(row) for row in input_tensor]
+    )
+    diffs = torch.diff(rows_with_drops_tensor, dim=2)
+    diff_is_consistent_mask = torch.all(diffs.isnan() | (diffs > 0), dim=2) | torch.all(
+        diffs.isnan() | (diffs < 0), dim=2
+    )
+    diff_abs_not_too_large_mask = torch.all(
+        diffs.isnan() | (torch.abs(diffs) <= 3), dim=2
+    )
+    is_valid_mask = diff_is_consistent_mask & diff_abs_not_too_large_mask
+    is_valid_row = torch.any(is_valid_mask, dim=1)
+    return is_valid_row.sum().item()
 
 
 def run():
     input_tensor = read_input()
-    print(input_tensor)
     part1_solution = part1(input_tensor)
     print(f"Part 1 solution: {part1_solution}")
 
     input_tensor = read_input()
     part2_solution = part2(input_tensor)
     print(f"Part 2 solution: {part2_solution}")
+
+    # Benchmarking part 2
+    print("Benchmarking...")
+    benchmark_iterations = 100
+    benchmark_s = timeit.timeit(
+        lambda: part2(input_tensor), number=benchmark_iterations
+    )
+    print(f"Part 1 benchmark: {benchmark_s / benchmark_iterations * 1000 :.2f} ms")
 
 
 if __name__ == "__main__":
